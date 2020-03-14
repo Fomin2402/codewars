7af8ced0883d3cba0c4cd59a654d887ade1cb2a8
Alex <alexfomin2402@gmail.com>
2020-03-14 18:11:06 +0300

    complete: Detect Pangram

---
+++ 6_kyu/ Detect_Pangram/index.js
@@ -0,0 +1,45 @@
+/*
+DESCRIPTION:
+    A pangram is a sentence that contains every single letter of the alphabet at least once.
+    For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
+    because it uses the letters A-Z at least once (case is irrelevant).
+    Given a string, detect whether or not it is a pangram.
+    Return True if it is, False if not. Ignore numbers and punctuation.
+
+EXAMPLE:
+    "The quick brown fox jumps over the lazy dog." - true
+    "This is not a pangram." - false
+*/
+
+let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+];
+const isPangram = string =>
+    !alphabet.filter(letter => !string.toLowerCase().includes(letter)).length;
+
+module.exports = isPangram;
